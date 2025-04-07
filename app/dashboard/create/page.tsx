01bae5203fa929c74f58c8bd7e5b7bcb7bf8e3de
SunilNeupane77 <sunilneupane957@gmail.com>
2025-04-07 13:19:42 +0545

    build error fixed of dashboard/create

--- app/dashboard/create/page.tsx
+++ app/dashboard/create/page.tsx
@@ -1,4 +1,4 @@
-import { handleSubission } from "@/app/actions";
+import { handleSubmission } from "@/app/actions";
 import { SubmitButton } from "@/components/general/SubmitButton";
 import {
   Card,
@@ -11,36 +11,51 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
 
-export default function Dashboard() {
+export default function DashboardCreatePage() {
   return (
-    <>
-      <div>
-        <Card className="max-w-lg mx-auto">
-          <CardHeader>
-            <CardTitle>Create Post</CardTitle>
-            <CardDescription>
-              Create a new post to share with the world
-            </CardDescription>
-          </CardHeader>
-          <CardContent>
-            <form className="flex flex-col gap-4" action={handleSubission}>
-              <div className="flex flex-col gap-2">
-                <Label>Title</Label>
-                <Input name="title" required type="text" placeholder="Title" />
-              </div>
-              <div className="flex flex-col gap-2">
-                <Label>Content</Label>
-                <Textarea name="content" required placeholder="Content" />
-              </div>
-              <div className="flex flex-col gap-2">
-                <Label>Image Url</Label>
-                <Input name="url" required type="url" placeholder="Image Url" />
-              </div>
-              <SubmitButton/>
-            </form>
-          </CardContent>
-        </Card>
-      </div> 
-    </>
+    <div>
+      <Card className="max-w-lg mx-auto">
+        <CardHeader>
+          <CardTitle>Create Post</CardTitle>
+          <CardDescription>
+            Create a new post to share with the world
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form className="flex flex-col gap-4" action={handleSubmission}>
+            <div className="flex flex-col gap-2">
+              <Label htmlFor="title">Title</Label>
+              <Input 
+                id="title"
+                name="title" 
+                required 
+                type="text" 
+                placeholder="Title" 
+              />
+            </div>
+            <div className="flex flex-col gap-2">
+              <Label htmlFor="content">Content</Label>
+              <Textarea 
+                id="content"
+                name="content" 
+                required 
+                placeholder="Content" 
+              />
+            </div>
+            <div className="flex flex-col gap-2">
+              <Label htmlFor="url">Image Url</Label>
+              <Input 
+                id="url"
+                name="url" 
+                required 
+                type="url" 
+                placeholder="Image Url" 
+              />
+            </div>
+            <SubmitButton />
+          </form>
+        </CardContent>
+      </Card>
+    </div>
   );
 }
